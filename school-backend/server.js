38302520f38191e4d63e29980d0f6fcf7d4177f0
Prem9634 <pk9236715@gmail.com>
2026-06-27 18:00:00 +0530

    Fix CROS

--- school-backend/server.js
+++ school-backend/server.js
@@ -21,7 +21,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: process.env.FRONTEND_URL || "http://yogeshschoolmanagement.free.je",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
